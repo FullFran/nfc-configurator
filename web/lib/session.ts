f0f6868116f1ca57c5ae84de10685acd492b4117
fullfran <franfi17sw@gmail.com>
2025-12-25 19:38:19 +0100

    chore: Set session cookie secure option to false.

--- web/lib/session.ts
+++ web/lib/session.ts
@@ -11,7 +11,7 @@ export const sessionOptions: SessionOptions = {
     password: process.env.SESSION_PASSWORD || "complex_password_at_least_32_characters_long",
     cookieName: "nfc_config_session",
     cookieOptions: {
-        secure: process.env.NODE_ENV === "production",
+        secure: false, // Temporarily set to false for debugging production issue
         maxAge: 60 * 60 * 24 * 7, // 1 week
         httpOnly: true,
         sameSite: "lax",
